8cd67070045678d374c8539481818c9173a9be3b
Zubair Ahmed <YOUR_GITHUB_EMAIL>
2026-09-23 00:59:00 +0500

    JavaScript Practice

---
+++ Coading-Practice-5/script.js
@@ -0,0 +1,71 @@
+// let expenses = [500, 1200, 300, 800, 1500];
+
+// const totalExpenses = expenses.reduce((items,total) => {
+//     return items += total
+// },0);
+// console.log(totalExpenses);
+
+// let expenses = [500, 1200, 300, 800, 1500];
+// let hightestExpenses = [];
+
+// for(let i = 0; i < expenses.length; i++){
+//     if (expenses[i] > 1000) {
+//         hightestExpenses.push(expenses[i])
+//     }
+// }
+
+// console.log(hightestExpenses);
+
+// let expenses = [
+//   {title: "Burger", category: "Food", amount: 500},
+//   {title: "Bus", category: "Transport", amount: 200},
+//   {title: "Pizza", category: "Food", amount: 800},
+//   {title: "Book", category: "Education", amount: 1000}
+// ];
+
+// let expensiveExpense = expenses[0];
+
+// for(let i = 0; i < expenses.length; i++){
+//     if (expenses[i].amount > expensiveExpense.amount) {
+//         expensiveExpense = expenses[i];
+//     }
+// }
+
+// console.log(expensiveExpense);
+
+// let income = 50000;
+
+// let expenses = [
+//   {title: "Food", amount: 5000},
+//   {title: "Transport", amount: 3000},
+//   {title: "Internet", amount: 2000}
+// ];
+
+// let total = 0;
+
+// for(let i = 0; i < expenses.length; i++){
+//     total += expenses[i].amount;
+// }
+
+// let balance = income - total;
+
+// console.log(`Total = ${total}`);
+// console.log(`Balance = ${balance}`);
+
+// let expenses = [
+//   {title: "Burger", category: "Food", amount: 500},
+//   {title: "Bus", category: "Transport", amount: 200},
+//   {title: "Pizza", category: "Food", amount: 800},
+//   {title: "Book", category: "Education", amount: 1000}
+// ];
+
+// let count = 0;
+
+// for(let i = 0; i < expenses.length; i++){
+//     if (expenses[i].category === "Food") {
+//         count++
+//     }
+// }
+
+// console.log(count);
+
