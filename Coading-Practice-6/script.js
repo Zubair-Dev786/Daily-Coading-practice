3781eb2224b377d2b80e84088a1713680c869f81
Zubair Ahmed <YOUR_GITHUB_EMAIL>
2026-09-24 14:27:46 +0500

    JavaScript Practice

---
+++ Coading-Practice-6/script.js
@@ -0,0 +1,55 @@
+// let products = [
+//   { name: "Laptop", price: 80000, stock: 5 },
+//   { name: "Mouse", price: 1500, stock: 0 },
+//   { name: "Keyboard", price: 3000, stock: 8 },
+//   { name: "Monitor", price: 25000, stock: 0 },
+//   { name: "Headphones", price: 5000, stock: 3 }
+// ];
+
+// let outofStcok = [];
+
+// for(let i = 0; i < products.length; i++){
+//     if (products[i].stock === 0) {
+//         outofStcok.push(products[i]);
+//     }
+// }
+
+// console.log(outofStcok);
+
+// let cart = [
+//   { name: "Laptop", price: 80000, quantity: 1 },
+//   { name: "Mouse", price: 1500, quantity: 2 },
+//   { name: "Keyboard", price: 3000, quantity: 3 },
+//   { name: "Headphones", price: 5000, quantity: 2 }
+// ];
+
+// let grandTotal = 0;
+
+// for (let i = 0; i < cart.length; i++) {
+//     const item = cart[i];
+
+//     let total = cart[i].price * cart[i].quantity;
+//     grandTotal += total;
+// }
+
+// console.log(grandTotal);
+
+// let students = [
+//   { name: "Ali", marks: 78 },
+//   { name: "Ahmed", marks: 45 },
+//   { name: "Hamza", marks: 91 },
+//   { name: "Usman", marks: 32 },
+//   { name: "Bilal", marks: 67 }
+// ];
+
+// let passedStudent = [];
+
+// for (let i = 0; i < students.length; i++) {
+    
+//     if (students[i].marks >= 50) {
+//         passedStudent.push(students[i].name);
+//     }
+// }
+
+// console.log(passedStudent);
+
