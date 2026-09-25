0819bf2c1d9e2464ec58196523f852aab3c0a869
Zubair Ahmed <YOUR_GITHUB_EMAIL>
2026-09-25 21:25:33 +0500

    JavaScript Practice

---
+++ Coading-Practice-7/script.js
@@ -0,0 +1,51 @@
+// let expenses = [1200, 500, 800, 1500, 300];
+// let total = 0;
+
+// for(let i = 0; i < expenses.length; i++){
+//     total += expenses[i];
+// }
+
+// console.log(total);
+
+// let income = 50000;
+// let expenses = [12000, 5000, 3500, 2000, 1500];
+
+// let total = 0;
+// let balance = 0;
+
+// for(let i = 0; i < expenses.length; i++){
+//     total += expenses[i];
+//     balance = income - total
+// }
+
+// console.log(total);
+// console.log(balance);
+
+// let budget = 30000;
+// let expenses = [8000, 4500, 7000, 3000, 2500];
+
+// let total = 0;
+
+// for(let i = 0; i < expenses.length; i++){
+//     total += expenses[i];
+// }
+
+// if (total <= budget) {
+//     console.log("Within Budget");
+// } else {
+//     console.log("Budget Exceeded");
+// }
+
+// console.log("Total Expenses: " +total);
+
+// let expenses = [1200, 5000, 800, 3500, 1500, 7000, 2200];
+// let highestExpenses = expenses[0];
+
+// for(let i = 0; i < expenses.length; i++){
+//     if (expenses[i] > highestExpenses) {
+//         highestExpenses = expenses[i];
+//     }
+// }
+
+// console.log(highestExpenses);
+
